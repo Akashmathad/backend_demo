8620f934f6b47184c0cce1b4e79849ceb80194df
Akash Mathad <akashmathad002@gmail.com>
2023-11-24 09:20:29 +0530

    changes

---
+++ practice.js
@@ -0,0 +1,9 @@
+console.log(
+  JSON.stringify(`public class UserProgram {
+  public static void main(String[] args) {
+      int a = 52;
+      int b = 502;
+      System.out.println("Ans is: " +(a + b));
+  }
+}`)
+);
